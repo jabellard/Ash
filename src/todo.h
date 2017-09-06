6a6c6d1896e5f63259255d4d1b1394bb5e56c98e
joenatech7 <joenatech@gmail.com>
2017-09-06 14:04:17 -0400

    added todo file

---
+++ src/todo.h
@@ -0,0 +1,37 @@
+/**
+* @file todo.h
+* @author Joe Nathan Abellard {https://github.com/joenatech7}
+* 
+* @date September 1, 2017
+* @brief Contains a todo list.
+*
+* @par Description
+* Contains a todo list.
+*
+* @sa
+* #TODO
+* 
+*/
+
+
+
+/**
+* @brief Todo list.
+*
+* @par Description
+* Todo list.
+*
+* @todo
+* Add a code liscense.
+*
+* @todo
+* Handle error caused by C-D.
+*
+* @todo
+* If pipeline k in pipelinelist of size n > k, ignore all k+1..n pipelines.
+*
+* @todo
+*
+* Make sure callgraph option 99orks 99hen using codedocs
+*/
+#define TODO 1
